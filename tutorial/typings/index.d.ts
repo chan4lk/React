6eff57a764aeeeafe7ef050d87f61804d3650555
Chandima Ranaweera <chandima@chandimaranaweera.com>
2016-10-02 20:24:57 +0530

    react tutorial added.

---
+++ tutorial/typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/expect/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/react-addons-test-utils/index.d.ts" />
+/// <reference path="globals/react-dom/index.d.ts" />
+/// <reference path="globals/react/index.d.ts" />
